47d7e23eebb3f5c1f8f6236549ab2ee0d5102984
hide-a1 <hide210g@gmail.com>
2024-12-01 14:53:16 +0900

    初期化

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'infinite-scroll-by-intersection-observer' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('infinite-scroll-by-intersection-observer');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, infinite-scroll-by-intersection-observer');
-  });
 });
